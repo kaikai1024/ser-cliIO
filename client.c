23efefdbeada52a6488392d3be48b6446952ade3
kaikai1024 <281959824@qq.com>
2015-01-16 00:39:51 +0800

    Update client.c
    单线程

--- client.c
+++ client.c
@@ -1,70 +1,134 @@
-#include <stdio.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/socket.h>  
-#include <stdlib.h> 
-#include <string.h>   
-                        
-#define PORT 8888                                  
-#define REMOTE_IP "127.0.0.1"                    
+/*************************************************************************
+    > File Name: client.c
+    > Author: kkFeng
+    > Created Time: 2015年05月11日 
+ ************************************************************************/
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<netdb.h>
+#include<stdarg.h>
+#include<string.h>
 
-int  main(int argc,char *argv[])
-{ 
-   	int s ;                                         
-   	struct sockaddr_in addr ;
-  	char mybuffer[256];
-	char *	sendBuffer = "hello";
+#define SERVER_PORT 8000
+#define BUFFER_SIZE 1024
+#define FILE_NAME_MAX_SIZE 512
 
-   	if( (s=socket(AF_INET,SOCK_STREAM,0))<0 )    
-   	{
-      		perror("socket");
-      		exit(1);
+/* 包头 */
+typedef struct 
+{
+	int id;
+	int buf_size;
+}PackInfo;
 
-   	}
-   	else
-   	{
-     	 	printf("socket created .\n");
-      		printf("socked id: %d \n",s);
+/* 接收包 */
+struct RecvPack
+{
+	PackInfo head;
+	char buf[BUFFER_SIZE];
+} data;
 
-   	}
-   	bzero(&addr,sizeof(addr));                     
-   	addr.sin_family =AF_INET;
-   	addr.sin_port=htons(PORT);
-   	addr.sin_addr.s_addr=inet_addr(REMOTE_IP);
 
-   	if(connect(s, (struct sockaddr *)&addr,sizeof(addr))<0)            
+int main()
+{
+	//int id = 1;
+	/* 发送id */
+	int send_id = 0;
 
-   	{
+	/* 接收id */
+	int receive_id = 0;
+	/* 服务端地址 */
+	struct sockaddr_in server_addr;
+	bzero(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server_addr.sin_port = htons(SERVER_PORT);
+	socklen_t server_addr_length = sizeof(server_addr);
 
-      		perror("connect");
-      		exit(1);
+	/* 创建socket */
+	int client_socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(client_socket_fd < 0)
+	{
+		perror("Create Socket Failed:");
+		exit(1);
+	}
 
-   	}
-   	else
-  	{
+	/* 输入文件名到缓冲区 */
+	char file_name[FILE_NAME_MAX_SIZE+1];
+	bzero(file_name, FILE_NAME_MAX_SIZE+1);
+	printf("Please Input File Name to Server: ");
+	scanf("%s", file_name);
 
-      		printf("connected ok!\n");
-      		printf("remote ip:%s\n",REMOTE_IP);
-      		printf("remote port:%d\n",PORT);
-    	}
-	while(1)                                         
-   	{
-      		bzero(mybuffer,sizeof(mybuffer));               
-      		if(send(s,sendBuffer,sizeof(sendBuffer),0)<0)     
-      		{
-         		perror("send");                            
-         		exit(1);
-      		}
-      		else
-      		{
-			printf("send:%s\n",sendBuffer); 
-       			bzero(mybuffer,sizeof(mybuffer));       
-         		recv(s ,mybuffer,sizeof(mybuffer),0);      
-         		printf("received:%s\n",mybuffer);         
-      		}
-   	}
+	char buffer[BUFFER_SIZE];
+	bzero(buffer, BUFFER_SIZE);
+	strncpy(buffer, file_name, strlen(file_name)>BUFFER_SIZE?BUFFER_SIZE:strlen(file_name));
+
+	/* 发送文件名 */
+	if(sendto(client_socket_fd, buffer, BUFFER_SIZE,0,(struct sockaddr*)&server_addr,server_addr_length) < 0)
+	{
+		perror("Send File Name Failed:");
+		exit(1);
+	}
+
+	/* 打开文件 */
+	//FILE *fp = fopen(file_name, "r");
+	FILE *fp = fopen("/home/kkf/1G.img", "r");
+	if(NULL == fp)
+	{
+		printf("File:%s Not Found.\n", file_name);
+	}
+	else
+	{
+		int len = 0;
+		/* 每读取一段数据，便将其发给server */
+		while(1)
+		{
+			PackInfo pack_info;
+
+			if(receive_id == send_id)
+			{
+				++send_id;
+				if((len = fread(data.buf, sizeof(char), BUFFER_SIZE, fp)) > 0)
+				{
+					data.head.id = send_id;  /* 发送id放进包头,用于标记顺序 */
+					data.head.buf_size = len;  /* 记录数据长度 */
+					if(sendto(client_socket_fd, (char*)&data, sizeof(data), 0, (struct sockaddr*)&server_addr, 								server_addr_length) < 0)
+					{
+						perror("Send File Failed:");
+						break;
+					}
+					/* 接收确认消息 */
+					recvfrom(client_socket_fd, (char*)&pack_info, sizeof(pack_info), 0, (struct sockaddr*)&server_addr, 								&server_addr_length);
+					receive_id = pack_info.id;	
+				}
+				else
+				{
+					break;
+				}
+			}
+			else
+			{
+				/* 如果接收的id和发送的id不相同,重新发送 */
+				if(sendto(client_socket_fd, (char*)&data,sizeof(data),0,(struct sockaddr*)&server_addr, server_addr_length) < 0)
+				{
+					perror("Send File Failed:");
+					break;
+				}
+				/* 接收确认消息 */
+				recvfrom(client_socket_fd, (char*)&pack_info, sizeof(pack_info), 0, (struct sockaddr*)&server_addr, 									&server_addr_length);
+				receive_id = pack_info.id;	
+			}
+		}
+		/* 关闭文件 */
+		fclose(fp);
+		printf("File:%s Transfer Successful!\n", file_name);
+	}
+
+	close(client_socket_fd);
+	return 0;
 }
