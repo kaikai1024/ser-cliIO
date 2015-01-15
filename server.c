93d4e0fa48c5396e9a98feb28458cfbee52fbfb1
kaikai1024 <281959824@qq.com>
2015-01-15 12:03:33 +0800

    Update server.c
    多线程

--- server.c
+++ server.c
@@ -1,132 +1,177 @@
-#include <sys/socket.h>
-#include <sys/epoll.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h> 
-#define PORT 8888
-#define MAXFDS 5000
-#define EVENTSIZE 100
-int epfd;
-void *serv_epoll(void *p);
-void setnonblocking(int fd) 
-{
-	int opts;
-	opts = fcntl(fd, F_GETFL);
-	if (opts < 0) 
-	{
-		fprintf(stderr, "fcntl failed\n");
-		return;
-	}
-	opts = opts | O_NONBLOCK;
-	if (fcntl(fd, F_SETFL, opts) < 0) 
-	{
-		fprintf(stderr, "fcntl failed\n");
-		return;
-	}
-	return;
-}
 
-int main(int argc, char *argv[]) 
+#include<stdio.h> 			/* perror */
+#include<stdlib.h>			/* exit	*/
+#include<sys/types.h>		/* WNOHANG */
+#include<sys/wait.h>		/* waitpid */
+#include<string.h>			/* memset */
+//
+#include <sys/time.h>
+#include <pthread.h>
+//
+#include "socketwrapper.h" 	/* socket layer wrapper */
+
+#define	true		1
+#define false		0
+
+#define MYPORT 		3490                /* 监听的端口 */ 
+#define BACKLOG 	10                 	/* listen的请求接收队列长度 */
+//
+int sendexit=0;		//控制发送线程状态的全局变量
+int recvexit=0;		//控制接收程状态的全局变量
+int filesize=1;		//记录文件大小的全局变量
+int recvsize=0;		//记录接收文件大小的全局变量
+int id=1;
+
+int sockfd;            /* 监听端口，数据端口 */
+char * name = NULL;	//记录文件名
+
+struct baohead		//包头
 {
-	int fd, cfd, opt = 1;
-	struct epoll_event ev;
-	struct sockaddr_in sin, cin;
-	socklen_t sin_len = sizeof(struct sockaddr_in);
+int size;
+int id;
+int recvsize;
+};
+//typedef baohead ElemType;
+struct baohead datahead;
+struct recvbuf		//包格式
+{
+//ElemType head;		//包头
+struct baohead head;
+char buf[1024];		//存放数据的变量
+int bufSize;		//存放数据长度的变量
+
+};
+struct recvbuf data;
+
+void* recvfunc(void* p);		//接收线程
+//
+int theiraddr_len ;
+struct sockaddr_in sa;         /* 自身的地址信息 */ 
+struct sockaddr_in their_addr; /* 连接对方的地址信息 */ 
+
+int main() 
+{
+
+	//
 	pthread_t tid;
 	pthread_attr_t attr;
-	epfd = epoll_create(MAXFDS);
-	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) <= 0) 
+	//
+	if ((sockfd = Socket(AF_INET, SOCK_DGRAM, 0)) == -1) 
 	{
-		fprintf(stderr, "socket failed\n");
-		return -1;
+		perror("socket"); 
+		exit(1); 
 	}
-	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (const void*) &opt, sizeof(opt));
-	memset(&sin, 0, sizeof(struct sockaddr_in));
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons((short) (PORT));
-	sin.sin_addr.s_addr = INADDR_ANY;
-	if (bind(fd, (struct sockaddr *) &sin, sizeof(sin)) != 0) 
+	//setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (const void*) &opt, sizeof(opt));
+
+	sa.sin_family = AF_INET;
+	sa.sin_port = Htons(MYPORT);         /* 网络字节顺序 */
+	sa.sin_addr.s_addr = INADDR_ANY;     /* 自动填本机IP */
+	memset(&(sa.sin_zero),0, 8);            /* 其余部分置0 */
+	theiraddr_len = sizeof(their_addr);
+
+	if ( Bind(sockfd, (struct sockaddr *)&sa, sizeof(sa)) == -1) 
 	{
-		fprintf(stderr, "bind failed\n");
-		return -1;
+		perror("bind");
+		exit(1);
 	}
-	if (listen(fd, 32) != 0) 
-	{
-		fprintf(stderr, "listen failed\n");
-		return -1;
-	}
+	recvfrom(sockfd, name, 100, 0,(struct sockaddr *)&their_addr,&theiraddr_len);
+	recvfrom(sockfd, &filesize, 100, 0,(struct sockaddr *)&their_addr,&theiraddr_len);
+	printf("receive the file :%s,with the size :%d",name,filesize);
+       	//r = fcntl(sockfd, F_GETFL, 0);
+       	//fcntl(sockfd, F_SETFL, r & ~O_NONBLOCK);
+
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-	if (pthread_create(&tid, &attr, serv_epoll, NULL) != 0) 
+	if (pthread_create(&tid, &attr, recvfunc, NULL) != 0) 
 	{
 		fprintf(stderr, "pthread_create failed\n");
 		return -1;
 	}
-	while (1) 
+	while(recvexit<2)//如果接收线程未退出，则主线程睡一段时间。
 	{
-		cfd = accept(fd, (struct sockaddr *) &cin, &sin_len);
-		if (cfd <= 0) 
-		{
-			sleep(1);
-			continue;
-		}
-		setnonblocking(cfd);
-		ev.data.fd = cfd;
-		ev.events = EPOLLIN | EPOLLET;
-		if (epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &ev) < 0) 
-		{
-			close(cfd);
-		}
-		printf("connect from %s\n",inet_ntoa(cin.sin_addr));
-		printf("cfd=%d\n",cfd);
+		//Sleep(100);
 	}
-	if (fd > 0)
-		close(fd);
+	printf("退出成功!\n");
 	return 0;
 }
-void *serv_epoll(void *p) 
+void* recvfunc(void* p)
 {
-	int i, ret, cfd, nfds;
-	struct epoll_event ev, events[EVENTSIZE];
-	char buffer[512];
-	char * sendBuffer = "hi";
-	while (1) 
+	recvexit=1;	//告诉主线程，接收线程已经创建成功。
+	FILE * fp;
+	fp=fopen("/home/kkf/recv.img", "wb");//如果原文件存在，则将原来文件删除。并新建一个空文件
+	fclose(fp);
+	fp=fopen("/home/kkf/recv.img", "ab");//打开那个空文件。
+	fd_set fdread;//建立集合
+	while( recvexit==1 )
 	{
-		nfds = epoll_wait(epfd, events, EVENTSIZE, -1);
-		printf("nfds ........... %d\n",nfds);
-		for (i = 0; i < nfds; i++) 
+		FD_ZERO(&fdread);//清空集合
+		FD_SET(sockfd,&fdread);//将socket放进集合内
+		struct timeval val;//设置超时时间
+		int Result=select(0,&fdread,0,0,&val);//检查sokcet的可读性
+		if(Result==-1)//错误则退出线程
 		{
-			if (events[i].events & EPOLLIN) 
+			printf("select error %s\n",strerror(errno));
+                        exit(-1);
+		}
+		else if(Result==0)//没数据到则马上返回。
+		{
+			printf("select timeout,continue circle\n");
+			continue;
+		}
+	
+		int rec = recvfrom(sockfd, (char *)(&data), 2048, 0,(struct sockaddr *)&their_addr,&theiraddr_len);//读取到来的数据报
+		if( rec > 0 )
+		{
+			if(data.head.id == id)//包ID是所期望的,则读入数据
 			{
-				bzero(buffer,sizeof(buffer)); 
-				cfd = events[i].data.fd;
-				ret = recv(cfd, buffer, sizeof(buffer), 0);
-				//printf("read ret..........= %d\n",ret);
-				printf("received:%s\n",buffer);
-				ev.data.fd = cfd;
-				ev.events = EPOLLOUT | EPOLLET;
-				epoll_ctl(epfd, EPOLL_CTL_MOD, cfd, &ev);
-			} 
-			else if (events[i].events & EPOLLOUT) 
-			{
-				cfd = events[i].data.fd;
-				ret = send(cfd, sendBuffer, strlen(sendBuffer), 0);
-				//printf("send ret...........= %d\n", ret);
-				printf("send:%s\n",sendBuffer);
-				ev.data.fd = cfd;
-				ev.events = EPOLLIN | EPOLLET;
-				epoll_ctl(epfd, EPOLL_CTL_MOD, cfd, &ev);
-				//shutdown(cfd, 1);
-				//if (cfd > 0)
-					//close(cfd);
+				// printf("id=%d\n",data.head.id);
+				// printf("bufsize=%d\n",data.bufSize);
+				fwrite( data.buf, sizeof(char), data.bufSize, fp);
+				recvsize=recvsize+data.bufSize;//记录新的接收到的文件长度
+				// printf("recvsize=%d\n",recvsize);
+				filesize=data.head.size;
+				datahead.id= data.head.id;
+				datahead.recvsize=recvsize;
+				datahead.size=filesize;
+				//发送收到数据的确认包
+				int d=sendto(sockfd,(char*)(&datahead),sizeof(datahead),0,(struct sockaddr *)&their_addr,sizeof(their_addr));
+				if(d>0)
+				{
+					// printf("发送收到数据包的确认信息成功!\n");
+				}
+				id=id+1;
 			}
 		}
+		else if(data.head.id < id)//如果是已经接收过的包的重发包，则丢弃，并重发一次确认包告诉发送端该包已收。
+		{
+			datahead.id = data.head.id;
+			datahead.recvsize=recvsize;
+			datahead.size=filesize;
+			
+			int d=sendto(sockfd,(char*)(&datahead),sizeof(datahead),0,(struct sockaddr *)&their_addr,sizeof(their_addr));
+			if(d>0)
+			{
+				// printf("发送收到数据包的确认信息成功!\n");
+			}
+		
+		
+		}
+	
+		if(filesize == recvsize )//如果接收到的文件长度等于文件大小，
+		{ //等发送一个确认包告诉发送端文件已接收完成，并退出自己的接收线程
+			datahead.id = data.head.id;
+			datahead.recvsize=-1;
+			datahead.size=filesize;
+			printf("filesize=%d\n",filesize);
+			printf("recvsize=%d\n",recvsize);
+			printf("文件接收完成!\n");
+			int d=sendto(sockfd,(char*)(&datahead),sizeof(datahead),0,(struct sockaddr *)&their_addr,sizeof(their_addr));
+			sendexit=2;
+			recvexit=2;
+			break;
+		}
 	}
+	fclose(fp);
+	recvexit=2;
 	return NULL;
 }
